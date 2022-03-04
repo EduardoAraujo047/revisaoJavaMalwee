1a48300e22ed568f48dcd45f9c91bfae456f2ae5
EduardoAraujo047 <eduardoaraujo2609@gmail.com>
2022-03-04 16:26:24 -0300

    Revisão de conteúdo em Java

---
+++ src/br/com/senai/view/ProgramaPrincipal.java
@@ -0,0 +1,56 @@
+package br.com.senai.view;
+
+import br.com.senai.controller.DadosController;
+
+import java.util.Scanner;
+
+public class ProgramaPrincipal {
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+        DadosController dadosController = new DadosController();
+        int opcao;
+        String continuar = "s";
+
+        do {
+            System.out.println("+---------------------------+");
+            System.out.println("|Informe a opção desejada:  |");
+            System.out.println("|1) Cadastrar pessoa        |");
+            System.out.println("|2) Listar pesssoas         |");
+            System.out.println("|3) Editar dados de pessoas |");
+            System.out.println("|4) Excluir pessoa          |");
+            System.out.println("|9) Sair do Sistema         |");
+            System.out.println("+---------------------------+");
+            System.out.print("-> ");
+
+            opcao = tec.nextInt();
+
+            switch (opcao) {
+                case 1:
+
+                    dadosController.cadastrarPessoa();
+                    break;
+                case 2:
+                    dadosController.listarPessoa();
+                    break;
+                case 3:
+                    dadosController.editarPessoa();
+                    break;
+                case 4:
+                    dadosController.deletarPessoa();
+                    break;
+                case 9:
+                    System.out.println("Sistema Finalizado!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opção inváldia!");
+                    break;
+            }
+
+            System.out.println("Deseja realizar outra operação? [s/n]");
+            continuar = tec.next();
+
+        }while(continuar.equalsIgnoreCase("s"));
+        System.out.println("Sistema Finalizado!");
+    }
+}
